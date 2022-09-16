e84cb09e1e66379f4685671334eab5472c74f6cc
martins <gabrielamspinola@gmail.com>
2022-09-16 15:56:55 -0300

    chore: criação de acesso index

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,4 +1,4 @@
-const productRouter = require('./productRouter');
-const salesRouter = require('./salesRouter');
+const productRouter = require('./product.router');
+const salesRouter = require('./sales.router');
 
 module.exports = { productRouter, salesRouter };
